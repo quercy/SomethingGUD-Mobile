bd80582205336cf5c9fbf12b3b878c6646621ff9
quercy <reid.savage@gmail.com>
2015-05-02 17:21:38 -0400

    added rudimentary datepicker

--- js/site.js
+++ js/site.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
     var menu = '<div data-role="panel" id="menu-left" data-display="push" data-theme="a" data-position="left"> <ul data-role="listview"> <li><a href="#browse" data-rel="close">Browse Items</a></li> <li><a href="#cart" data-rel="close">Shopping cart</a></li> <li><a href="#" data-rel="close">Orders</a></li> <li><a href="#" data-rel="close">Account Information</a></li> </ul> </div>';
     var $sign_in_form = $("#sign-in-form");
     var $register_form = $("#register-form");
+    setTimeout(function () {   window.scrollTo(0, 1); }, 1000);
     //var submitLogin = function(data, callback) {
     //    console.log(data);
     //    callback();
@@ -22,6 +23,66 @@ $(document).ready(function() {
         $.mobile.pageContainer.prepend(menu);
         $("#menu-left").panel().enhanceWithin();
     });
+
+    //$sign_in_form.on('login', function() {
+    //    $("#item-grid").append('<div class="ui-block-b"> <div class="ui-body ui-body-d"> Block 2</div> </div>');
+    //});
+
+
+    $sign_in_form.on('login', function() {
+        BrowseController();
+    });
+
+    var BrowseController = (function() {
+        var _this = this;
+        var $grid = $("#item-grid");
+        var $date_display = $("#display-date");
+        var shopping_date = new Date(Date.now());
+        var $change_date_link = $("#change-date");
+        var $date_pick_div = $("#pick-date");
+
+        // constructor -- this isn't the JS OO way to do it, but I'll figure it out later
+        var constructor = function () {
+            //$date_display.data('','');
+            console.log(shopping_date.toDateString());
+            //update_date(Date.now());
+            update_date();
+            $date_pick_div.date({
+                onSelect: function(dateText) {
+                    update_date($date_pick_div.date("getDate"));
+                    $date_pick_div.slideUp();
+                }
+            }).hide();
+            $change_date_link.on('click', function(e) {
+                e.preventDefault();
+                $date_pick_div.slideDown();
+            });
+        };
+
+        var update_date = function(date_string) {
+            if(!(date_string == '' || date_string == undefined)) {
+                shopping_date = new Date(date_string);
+                console.log("trying to set date to " + date_string);
+            }
+            $date_display.text(shopping_date.toDateString());
+        };
+
+        var tableUpdate = function (category) {
+            // clear old items
+            // AJAX in new items
+
+            return {
+                // return the AJAX data
+            }
+
+        };
+        constructor();
+
+        // Public methods
+        return {
+            getTableData : function(){return tableUpdate();}
+        }
+    }());
 
     $sign_in_form.validate({
         rules: {
@@ -47,14 +108,16 @@ $(document).ready(function() {
                     dataType: 'json',
                     data: submit_data
                 }).
-                success( function(data) {
+                success(function(data) {
                     // sign in
+                    //$sign_in_form.trigger('login');
                     console.log('success' + data);
                 }).
                 error( function(data) {
                     console.log('error ' + data);
                 }).
                 always( function() {
+                    $sign_in_form.trigger('login');
                     console.log('always');
                     setTimeout(function() {
                         $.mobile.loading('hide', {
@@ -113,17 +176,19 @@ $(document).ready(function() {
                 }).
                 success( function(data) {
                     // sign in
+                    //$sign_in_form.trigger('login');
                     console.log('success' + data);
                 }).
                 error( function(data) {
                     console.log('error ' + data);
                 }).
                 always( function() {
+                    $sign_in_form.trigger('login');
                     console.log('always');
                     setTimeout(function() {
                         $.mobile.loading('hide', {
                         });
-                        $.mobile.navigate('#home');
+                        $.mobile.navigate('#browse');
                     }, 600);
                 });
         }
